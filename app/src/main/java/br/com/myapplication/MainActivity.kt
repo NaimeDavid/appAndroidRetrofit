5044f033d46b6271c1bfca05abdc8265a2039270
Naime David <naimedavid@gmail.com>
2022-03-03 16:26:46 -0300

    cricao

---
+++ app/src/main/java/br/com/myapplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package br.com.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val originText = findViewById<TextView>(R.id.textOi)
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val newText = editText.text.toString()
+            if (newText.isNotBlank()) {
+                //Se não tá em branco, o 'text' recebe o conteudo no originText
+                originText.text = "Olá "+ newText+" !"
+
+            }else{
+                editText.error = "Digite seu nome aqui"
+            }
+        }
+    }
+}
